9be5a632e91913327f6a25cf4fb85e644292c812
evaldas23 <evaldas.cerniauskas@gmail.com>
2020-08-02 00:41:45 +0300

    Add styles

--- scripts/todo-functions.js
+++ scripts/todo-functions.js
@@ -5,8 +5,8 @@ const getSavedTodos = () => {
     const todosJSON = localStorage.getItem('todos')
 
     try {
-   return todosJSON ? JSON.parse(todosJSON) : []
-    } catch (error) {
+        return todosJSON ? JSON.parse(todosJSON) : []
+    } catch (e) {
         return []
     }
 }
@@ -19,6 +19,7 @@ const saveTodos = (todos) => {
 // Remove todo by id
 const removeTodo = (id) => {
     const todoIndex = todos.findIndex((todo) => todo.id === id)
+
     if (todoIndex > -1) {
         todos.splice(todoIndex, 1)
     }
@@ -27,6 +28,7 @@ const removeTodo = (id) => {
 // Toggle the completed value for a given todo
 const toggleTodo = (id) => {
     const todo = todos.find((todo) => todo.id === id)
+
     if (todo) {
         todo.completed = !todo.completed
     }
@@ -34,7 +36,7 @@ const toggleTodo = (id) => {
 
 // Render application todos based on filters
 const renderTodos = (todos, filters) => {
-    const filteredTodos = todos.filter(function (todo) {
+    const filteredTodos = todos.filter((todo) => {
         const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
         const hideCompletedMatch = !filters.hideCompleted || !todo.completed
         
@@ -88,5 +90,5 @@ const generateTodoDOM = (todo) => {
 const generateSummaryDOM = (incompleteTodos) => {
     const summary = document.createElement('h2')
     summary.textContent = `You have ${incompleteTodos.length} todos left`
-    return summary 
-} 
+    return summary
+}
